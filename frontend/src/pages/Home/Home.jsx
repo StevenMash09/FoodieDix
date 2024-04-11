b9534ae514cc34e7e266b0d1e7b5c7a666090c9c
thakordixit567 <103193972+thakordixit567@users.noreply.github.com>
2024-04-11 14:35:00 +0530

    UPDATE!

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+import Header from "../../components/Header/Header";
+import Expmenu from "../../components/Exploremenu/Expmenu";
+
+const Home = () => {
+  return <div>
+   <Header/>
+   <Expmenu/>
+  </div>;
+};
+
+export default Home;
