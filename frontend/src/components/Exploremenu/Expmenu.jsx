8a9653c1cf76a664c24d1be49bc62f7868db53be
thakordixit567 <103193972+thakordixit567@users.noreply.github.com>
2024-04-13 15:23:05 +0530

    UPDATE!

--- frontend/src/components/Exploremenu/Expmenu.jsx
+++ frontend/src/components/Exploremenu/Expmenu.jsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import './Expmenu.css';
-import { menu_list } from '../../assets/assets';
+import React from "react";
+import "./Expmenu.css";
+import { menu_list } from "../../assets/assets";
 
-const Expmenu = () => {
+const Expmenu = ({ category, setCategory }) => {
   return (
-    <div className='explore-menu' id='explore-mrnu'>
+    <div className="explore-menu" id="explore-mrnu">
       <h1>Explore our menu</h1>
-      <p className='explore-menu-text'>Choose from a diverse menu featuring an array of delectable dishes to satisfy every palate.</p>
+      <p className="explore-menu-text">
+        Choose from a diverse menu featuring an array of delectable dishes to
+        satisfy every palate.
+      </p>
       <div className="explore-menu-list">
-        {menu_list.map((item,index) => {
-            return (
-                <div key={index} className="explore-menu-list-item">
-                   <img src={item.menu_image} alt="" />
-                   <p>{item.menu_name}</p>
-                </div>
-            )
+        {menu_list.map((item, index) => {
+          return (
+            <div
+              onClick={() =>
+                setCategory((prev) =>
+                  prev === item.menu_name ? "All" : item.menu_name
+                )
+              }
+              key={index}
+              className="explore-menu-list-item"
+            >
+              <img
+                className={category === item.menu_name?"active":""}
+                src={item.menu_image}
+                alt=""
+              />
+              <p>{item.menu_name}</p>
+            </div>
+          );
         })}
       </div>
+      <hr />
     </div>
-  )
-}
+  );
+};
 
-export default Expmenu
+export default Expmenu;
